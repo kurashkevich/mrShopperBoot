3a5805ee8fccd03b0009eaf8235ddaa0ffe87f72
Vladislav_K <v.kurashkevich@gmail.com>
2018-04-19 11:04:38 +0300

    some services for User

--- src/main/java/com/mrshopper/project/controller/UserController.java
+++ src/main/java/com/mrshopper/project/controller/UserController.java
@@ -1,7 +1,26 @@
 package com.mrshopper.project.controller;
 
+import com.mrshopper.project.entity.User;
+import com.mrshopper.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/save")
+    public User saveUser(){
+        return userService.save(new User(null, "stas", "konkov", "kon'", "pass", "konkov@mail.ru"));
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
 }
--- src/main/java/com/mrshopper/project/service/UserService.java
+++ src/main/java/com/mrshopper/project/service/UserService.java
@@ -1,11 +1,23 @@
 package com.mrshopper.project.service;
 
+import com.mrshopper.project.entity.User;
 import com.mrshopper.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    public User save(User user){
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers(){
+        return  userRepository.findAll();
+    }
 }
