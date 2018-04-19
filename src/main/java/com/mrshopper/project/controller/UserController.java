b109ec28a99d1aeab69c4b40703bdcae385dc71b
Vladislav_K <v.kurashkevich@gmail.com>
2018-04-19 11:27:53 +0300

    find by id

--- src/main/java/com/mrshopper/project/controller/UserController.java
+++ src/main/java/com/mrshopper/project/controller/UserController.java
@@ -4,6 +4,7 @@ import com.mrshopper.project.entity.User;
 import com.mrshopper.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,13 +15,26 @@ public class UserController {
     private UserService userService;
 
 
-    @GetMapping("/save")
-    public User saveUser(){
+    @GetMapping("/save/stas")
+    public User saveUserStas(){
         return userService.save(new User(null, "stas", "konkov", "kon'", "pass", "konkov@mail.ru"));
+    }
+    @GetMapping("/save/vlad")
+    public User saveUserVlad(){
+        return userService.save(new User(null, "vlad", "kurash", "vlad'", "pass", "kur@mail.ru"));
+    }
+    @GetMapping("/save/igor")
+    public User saveUserIgor(){
+        return userService.save(new User(null, "igot", "tishko", "igor'", "pass", "tish@mail.ru"));
     }
 
     @GetMapping("/users")
     public List<User> getAllUsers(){
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable Long id){
+        return userService.getUserById(id);
     }
 }
--- src/main/java/com/mrshopper/project/service/UserService.java
+++ src/main/java/com/mrshopper/project/service/UserService.java
@@ -20,4 +20,8 @@ public class UserService {
     public List<User> getAllUsers(){
         return  userRepository.findAll();
     }
+
+    public User getUserById(Long id){
+        return userRepository.findById(id).orElse(null);
+    }
 }
