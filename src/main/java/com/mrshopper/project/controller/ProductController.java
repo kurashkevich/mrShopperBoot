50678e73426318389e8d5736188bc6b70a996087
Vladislav_K <v.kurashkevich@gmail.com>
2018-04-24 12:25:43 +0300

    product controller fix non rest product controller delete

--- src/main/java/com/mrshopper/project/controller/ProductController.java
+++ src/main/java/com/mrshopper/project/controller/ProductController.java
@@ -1,12 +1,16 @@
 package com.mrshopper.project.controller;
 
+import com.mrshopper.project.entity.Detail;
 import com.mrshopper.project.entity.Product;
 import com.mrshopper.project.entity.User;
+import com.mrshopper.project.service.DetailService;
 import com.mrshopper.project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,30 +18,49 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
     @Autowired
-    private ProductService productService;
+    ProductService productService;
+    @Autowired
+    DetailService detailService;
 
-    @GetMapping(value = "/")
-    public List<Product> getProducts(){
-        return productService.getProducts();
+    @GetMapping
+    public String getProducts(Model model){
+        model.addAttribute("products", productService.getProducts());
+        return "product/main";
     }
 
-    @GetMapping("/{id}")
-    public Product getProductById(@PathVariable Long id){
-        return productService.getProductById(id);
+    @Transactional
+    @GetMapping("/delete")
+    public String delProduct(@RequestParam Long id){
+        System.out.println(id);
+        productService.deleteProduct(id);
+        return "product/main";
     }
 
-    @GetMapping("/by/{manufacturer}")
-    public List<Product> getProductsByManufacturer(@PathVariable String manufacturer){
-        return productService.getProductsByManufacturer(manufacturer);
+    @GetMapping("/create")
+    public String addProductForm(){
+        return "product/add-product-form";
     }
 
-    @GetMapping("/by/os/{os}")
-    public List<Product> findAllByDetailOs(@PathVariable String os){
-        return productService.findAllByDetailOs(os);
-    }
-    @GetMapping("/by/filter/{os}/{min, max}")
-    public List<Product> findAllByDetailOsAndPriceBetween(@PathVariable String os, @PathVariable double min, @PathVariable double max){
-        return productService.findAllByDetailOs(os);
+    @PostMapping("/create/product")
+    public String addProduct(@RequestParam String manufacturer,@RequestParam Double price, @RequestParam Integer count,
+                             @RequestParam String model, @RequestParam String os, @RequestParam String rom, @RequestParam String ram,
+                             @RequestParam String screenSize, @RequestParam String description){
+        Detail detail = new Detail();
+        detail.setModel(model);
+        detail.setOs(os);
+        detail.setRom(rom);
+        detail.setRam(ram);
+        detail.setScreenSize(screenSize);
+        detail.setDescription(description);
+        detailService.save(detail);
+
+        Product product = new Product();
+        product.setManufacturer(manufacturer);
+        product.setPrice(price);
+        product.setCount(count);
+        product.setDetail(detail);
+        productService.save(product);
+        return "product/main";
     }
 
 
