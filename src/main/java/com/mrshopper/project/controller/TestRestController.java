455f108aeab29c5d470c382057433c9e9b082db5
Vladislav_K <v.kurashkevich@gmail.com>
2018-04-25 17:31:23 +0300

    everything very very bad

---
+++ src/main/java/com/mrshopper/project/controller/TestRestController.java
@@ -0,0 +1,23 @@
+package com.mrshopper.project.controller;
+
+import com.mrshopper.project.entity.Product;
+import com.mrshopper.project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productsRest")
+public class TestRestController {
+    @Autowired
+    ProductService productService;
+
+    @GetMapping
+    List<Product> getRestSearchByFilter(@RequestParam String manufacturer,@RequestParam String osName, @RequestParam String minD, @RequestParam String maxD){
+        return productService.getProductsByFilter(manufacturer, osName, minD, maxD);
+    }
+}
--- src/main/webapp/assets/js/util.js
+++ src/main/webapp/assets/js/util.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $('#searchByFilter').click(function () {
         searchByFilterBtn();
+        //restSearchByFilterBtn();
     });
     $('#maxDiagonal').click(function () {
-       // var manufacturer = $( "#manufacturer option:selected" ).text();
-       // changeLink(manufacturer);
+
     });
 
 });
@@ -38,15 +38,52 @@ function searchByFilterBtn() {
     var maxRam =$('#maxRam').val();
 
     var url = filterLink(manufacturer, os, minDiagonal, maxDiagonal);
-    alert(url);
     $.ajax({
         headers:"Accept: application/json",
         type:'get',
         url: url
     }).done(function() {
-        alert('done');
-        $("#searchByFilter").attr("href", url);
+        //alert('done');
     })
     $("#searchByFilter").attr("href", url);
+    $("#manufacturer option:selected").attr("text", manufacturer);
+}
+
+
+function restFilterLink(manufacturer, os, minDiagonal, maxDiagonal){
+    var url = '/productsRest?manufacturer='+manufacturer+'&osName='+os+'&minD='+minDiagonal+'&maxD='+maxDiagonal;
+    return url;
+    //http://localhost:8080/productsRest?manufacturer=Samsung&osName=IOS&minD=3&maxD=7
+}
+
+function restSearchByFilterBtn() {
+    var manufacturer = $( "#manufacturer option:selected" ).text();
+    var os = $( "#os option:selected" ).text();
+    var minDiagonal =$('#minDiagonal').val();
+    var maxDiagonal =$('#maxDiagonal').val();
+
+
+    var product = {
+        manufacturer: manufacturer,
+        os: os,
+    };
+
+    var restUrl = filterLink(manufacturer, os, minDiagonal, maxDiagonal);
+    alert(restUrl);
+    $.ajax({
+        headers:"Accept: application/json",
+        type:'get',
+        dataType: "json",
+        url: restUrl
+    }).done(function(data) {
+        var el = ' <div id="2"> ' + data.manufacturer + '</div>  ';
+        $(".test").append(el);
+    })
+
 
 }
+
+/*
+<div class="test">
+    <div id="1">привет1  привет2  привет3</div>
+</div>*/
